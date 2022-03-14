c7c685aecdc1ee7baac45e22bcfb2c99d4994704
MeghaRathi-ee <75435222+MeghaRathi-ee@users.noreply.github.com>
2022-03-14 11:24:41 +0530

    Add files via upload

---
+++ introduction/print_function.py
@@ -0,0 +1,15 @@
+#print function
+
+print('hi')
+name='mei'
+print(name)
+
+print('hi',name)
+
+print('2,3,4')
+
+print(2,3,4)
+print(2,3,4,5,)
+
+print("hello, what's your name",end=' ')
+print(name)
